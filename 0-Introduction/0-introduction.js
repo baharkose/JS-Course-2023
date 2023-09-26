8782a483a02d458ba7e3f00d43421d8712abc87c
baharkose <baharkse17@gmail.com>
2023-09-26 14:51:11 +0300

    introduction

---
+++ 0-Introduction/0-introduction.js
@@ -0,0 +1,14 @@
+// alert('hello');
+// console.log('bahar');
+// console.log('reinvent yourself');
+// document.write('deneme');
+// window.alert('reinvent yourself');
+
+var a = 25;
+console.log(a);
+
+var myNumber = 5;
+console.log(myNumber);
+
+var a = b= 5;
+console.log(b);
